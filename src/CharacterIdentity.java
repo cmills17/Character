e988f453e0c91e910cef1084d8402a9001e6d221
Contessa Mills <cmills@deerfield.edu>
2016-02-10 20:41:31 -0500

    went back to old version, deleted lines trying to add the entities file

--- src/CharacterIdentity.java
+++ src/CharacterIdentity.java
@@ -16,81 +16,39 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-import com.opencsv.CSVReader;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 
 
 public class CharacterIdentity extends JFrame
-{ 
+{
 	
-	public static void main(String[] args) {
-		new CharacterIdentity();
-		
-	}
-		
-	Map<Integer,String> factss = new HashMap<Integer,String>(); 
-	Map<Integer,String> alpha = new HashMap<Integer,String>();
 	
-	public void loadfromCSV() 
-	{
-		try{
-		CSVReader reader = new CSVReader(new FileReader("/Users/cmills/Documents/workspace/Character/bin/entityfacts.csv"));
-	     String [] nextLine;
-	     while ((nextLine = reader.readNext()) != null) { //creates while loop--looks through the stuff in the map
-	    	 try{
-	    		 System.out.println(nextLine[1] + " " + nextLine[4]);
-	    		 factss.put(Integer.parseInt(nextLine[1], 16), nextLine[4]); //puts values into two lists-one is ints, one strings-matches them up
-	    	 }
-	    	 catch(Exception e){ //will let code work even after exception happens
-	    		 
-	    	 }
-	        // nextLine[] is an array of values from the line
-	     
-	     
-	     }
-		} 
-		catch(Exception e){
-			throw new RuntimeException(e); //will have code still run after all exceptions 
-		}
-	   
-	}
-	
-
-	public CharacterIdentity() {
-	super("project");
-		loadfromCSV();
+	public static void main(String[] args){
 		
 		
+		
+		JFrame frame = new JFrame ("project");
 		final JTextField textField = new JTextField(20);
-		this.getContentPane().add(textField, BorderLayout.CENTER); 
-		this.setVisible (true);
-		this.setSize(500,500);
+		frame.getContentPane().add(textField); 
+		frame.setVisible (true);
+		frame.setSize(500,500);
 		textField.setFont(new Font("Serif", Font.BOLD, 50)); 
 		final JLabel unicode = new JLabel ("unicode");
 		final JLabel entities = new JLabel("entities");
-		this.getContentPane().add(unicode, BorderLayout.NORTH);
-		this.getContentPane().add(entities, BorderLayout.SOUTH);
+		frame.getContentPane().add(unicode, BorderLayout.NORTH);
+		frame.getContentPane().add(entities, BorderLayout.SOUTH);
 		DocumentListener listener = new DocumentListener() { 
 	
+		
+		
+
+			@Override
 			public void insertUpdate(DocumentEvent e)
 			{
-				
 				String text = textField.getText(); 
 				int firstChar = text.charAt(0); //makes the first letter an "int"
-				String eng = factss.get(firstChar); //takes in the letter 
-				//eng = factss.get("");
-				unicode.setText(eng + "decimal and entity: " + firstChar + " " + "&#" + firstChar + "; " +"hex and hex entity: 0x" + String.format("%04x", firstChar) + " &#x" + String.format("%04x", firstChar) + ";");  //makes the decimal and hex value appear for the int
-			
-				entities.setText(eng);
+				unicode.setText("decimal and entity: " + firstChar + " " + "&#" + firstChar + "; " +"hex and hex entity: 0x" + String.format("%04x", firstChar) + " &#x" + String.format("%04x", firstChar) + ";");  //makes the decimal and hex value appear for the int
+				
 			}
-			
 
 		
 			public void removeUpdate(DocumentEvent e)
@@ -105,23 +63,12 @@ public class CharacterIdentity extends JFrame
 		
 				
 			}
-		   
-	
+			
 		};
 		textField.getDocument().addDocumentListener(listener); 
 		
-		 
 		
 		
 		
-		
-		
-		
-	
-	
-	
-	
+	}
 }
-}
-
-	
